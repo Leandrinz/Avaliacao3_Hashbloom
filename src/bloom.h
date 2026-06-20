99daee699f9560ec581206cccdad0e104237f80a
Jozenias Antônio <fayrghos@proton.me>
2026-06-20 17:24:41 -0300

    Adição de um filtro de bloom básico

--- src/bloom.h
+++ src/bloom.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "hash.h"
+
+/*
+ * Representa uma instância de um filtro de bloom.
+ */
+typedef struct {
+    int tamanho;
+    unsigned char *bytes;
+} Bloom;
+
+Bloom *iniciar_filtro(int tamanho);
+
+void liberar_filtro(Bloom *filtro);
+
+void inserir_filtro(Bloom *filtro, Usuario *usuario);
+
+bool consultar_filtro(Bloom *filtro, Usuario *usuario);
