fd6ad8628fa7913c8ed87bb3a0e414ddd541b152
viteihehe <victormanuelfernandesanacleto@gmail.com>
2026-06-26 17:10:21 -0300

    Atualizaçao do sistema de  testes por lote

--- programa/src/main.c
+++ programa/src/main.c
@@ -15,10 +15,11 @@ int main() {
     Bloom *filtro = iniciar_filtro(QUANT_ITENS * 10);
     Estatisticas estats = {};
 
-    // Testes
-    int *testes_id; // Armazena apenas o nome_int de cada usuário para o teste
-    int tamanho_teste = 10000;
-    testes_id = malloc(tamanho_teste * sizeof(int));
+    //Teste
+    bool carregado_tabela = false;
+    bool exibir_dados_teste = false;
+    Estatisticas dados_teste_bloom = {};
+    Estatisticas dados_teste_hash = {};
 
     limpar();
     puts("Bem-vindo ao Hashbloom!");
@@ -54,6 +55,9 @@ int main() {
                 // Verifica se o usuário desejado existe na tabela hash.
                 if (Busca(&tabela, *usuario_desejado).ocupado != -1) {
                     usuario_existe = true;
+                }
+                else {
+                    estats.falsos++;
                 }
 
                 estats.consultas_real++;
@@ -108,6 +112,9 @@ int main() {
                 if (Busca(&tabela, *usuario_desejado).ocupado != -1) {
                     usuario_existe = true;
                 }
+                else {
+                    estats.falsos++;
+                }
 
                 estats.consultas_real++;
                 estats.tempo_acc += calcular_tempo() - tempo_inicial;
@@ -128,24 +135,65 @@ int main() {
         case 3: {
             puts("ESTATÍSTICAS:");
 
-            if (estats.consultas_real != 0) {
-                estats.falsos_tx = (float)estats.falsos / estats.consultas_real;
-                estats.tempo_medio = estats.tempo_acc / estats.consultas_real;
-            }
+            
 
-            printf("    Elementos armazenados: %d\n", estats.armazenados);
-            printf("    Consultas realizadas na tabela: %d\n", estats.consultas_real);
-            printf("    Consultas evitadas pelo bloom: %d\n", estats.consultas_evit);
-            printf("    Falsos positivos: %d\n", estats.falsos);
-            printf("    Taxa de falsos positivos: %.2f%%\n", estats.falsos_tx * 100);
-            printf("    Tempo médio de consulta: %.1fms\n", estats.tempo_medio);
+            if(!exibir_dados_teste) {
+                if (estats.consultas_real != 0) {
+                    estats.falsos_tx = (float)estats.falsos / estats.consultas_real;
+                    estats.tempo_medio = estats.tempo_acc / estats.consultas_real;
+                    }
+
+                printf("    Elementos armazenados: %d\n", estats.armazenados);
+                printf("    Consultas realizadas na tabela: %d\n", estats.consultas_real);
+                printf("    Consultas evitadas pelo bloom: %d\n", estats.consultas_evit);
+                printf("    Falsos positivos: %d\n", estats.falsos);
+                printf("    Taxa de falsos positivos: %.2f%%\n", estats.falsos_tx * 100);
+                printf("    Tempo médio de consulta: %.1fms\n", estats.tempo_medio);
+            }
+            else {
+                puts("DADOS COM BLOOM:");
+                 if (dados_teste_bloom.consultas_real != 0) {
+                    dados_teste_bloom.falsos_tx = (float)dados_teste_bloom.falsos / dados_teste_bloom.consultas_real;
+                    dados_teste_bloom.tempo_medio = dados_teste_bloom.tempo_acc / dados_teste_bloom.consultas_real;
+                    }
+
+                printf("    Consultas realizadas na tabela: %d\n", dados_teste_bloom.consultas_real);
+                printf("    Consultas evitadas pelo bloom: %d\n", dados_teste_bloom.consultas_evit);
+                printf("    Falsos positivos: %d\n", dados_teste_bloom.falsos);
+                printf("    Taxa de falsos positivos: %.2f%%\n", dados_teste_bloom.falsos_tx * 100);
+                printf("    Tempo médio de consulta: %.1fms\n", dados_teste_bloom.tempo_medio);
+
+                puts("DADOS SEM BLOOM:");
+                if (dados_teste_hash.consultas_real != 0) {
+                    dados_teste_hash.tempo_medio = dados_teste_hash.tempo_acc / dados_teste_hash.consultas_real;
+                    }
+
+                printf("    Consultas realizadas na tabela: %d\n", dados_teste_hash.consultas_real);
+                printf("    Tempo médio de consulta: %.1fms\n", dados_teste_hash.tempo_medio);
+
+                puts("    [1] - Parar de exibir testes e resetar suas estatisticas");
+                puts("    [2] - Sair dessa aba (qualquer tecla)");
+                puts("");
+
+                int modo = 0;
+                printf("ESCOLHA: ");
+                scanf("%d", &modo);
+                if(modo == 1) {
+                    exibir_dados_teste = false;
+                    dados_teste_bloom = (Estatisticas){0};
+                    dados_teste_hash = (Estatisticas){0};
+                }
+            }
             break;
         }
 
         case 4: {
+            //Informa quais arquivos ja foram carregados e estao no sistema
+            
+
             puts("Selecione uma das opções abaixo:");
             puts("    [1] - Lote oficial");
-            puts("    [2] - Lote teste");
+            puts("    [2] - Realizar testes em lote");
             puts("");
 
             int modo = 0;
@@ -185,9 +233,24 @@ int main() {
 
                 puts("O arquivo foi lido com sucesso!");
                 fclose(lote);
+                carregado_tabela = true;
                 break;
             }
             case 2: {
+                //Criar estatistica proprias para teste de lote
+                if(!carregado_tabela) {
+                    puts("Falta os valores originais para realizar um teste");
+                    puts("Porfavor os insira na opçao 1 dessa pagina");
+                    break;
+                }
+                if(exibir_dados_teste) {
+                    puts("Seu teste esta pronto, pfvr visualize na aba estatistica");
+                    break;
+                }
+
+                int tamanho_teste = 0;
+                Usuario *valores_teste;
+
                 printf("Teste: ./dados/arquivos/");
 
                 char arquivo[40] = "";
@@ -202,21 +265,58 @@ int main() {
                     break;
                 }
 
-                int i = 0;
-                while (fscanf(lote, "%29s", nome_desejado) != EOF &&
-                       i < 10000) {
-                    Usuario *usuario_tmp = iniciarUsuario(nome_desejado);
-                    testes_id[i] =
-                        usuario_tmp
-                            ->nome_int; // Armazena estritamente o valor inteiro
-                    i++;
-                    free(usuario_tmp);
+                while (fscanf(lote, "%29s", nome_desejado) != EOF) {
+                    tamanho_teste++;
                 }
+                valores_teste = malloc(tamanho_teste*sizeof(Usuario));
+                //Retorna o ponteiro de leitura para o começo
+                rewind(lote);
 
-                tamanho_teste = i;
+                int pivo_temp = 0;
+                while (fscanf(lote, "%29s", nome_desejado) != EOF) {
+                    Usuario *temp = iniciarUsuario(nome_desejado);
+                    nomeToInt(temp);
+
+                    strcpy(valores_teste[pivo_temp].nome, temp->nome);
+                    valores_teste[pivo_temp].nome_int = temp->nome_int;
+                    valores_teste[pivo_temp].ocupado = temp->ocupado;
+                    pivo_temp++;
+                    free(temp);
+                }
 
                 puts("O arquivo foi lido com sucesso!");
                 fclose(lote);
+                          
+                    for(int i = 0; i < tamanho_teste; i++) {
+                        // Criamos uma struct temporária 
+                        Usuario temp = valores_teste[i];
+
+                        if (consultar_filtro(filtro, &temp) == false) {
+                            dados_teste_bloom.consultas_evit++;
+                        } else {
+                            float tempo_inicial1 = calcular_tempo();
+                            Usuario resultado = Busca(&tabela, temp);
+                            dados_teste_bloom.tempo_acc += calcular_tempo() - tempo_inicial1;
+
+                            if (resultado.ocupado == -1) {
+                                dados_teste_bloom.falsos++;
+                            }
+                            dados_teste_bloom.consultas_real++;
+                        }
+
+                        float tempo_inicial2 = calcular_tempo();
+                            Usuario resultado = Busca(&tabela, temp);
+                            dados_teste_hash.tempo_acc += calcular_tempo() - tempo_inicial2;
+
+                            if (resultado.ocupado != -1) {
+                                // Encontrado
+                            }
+                            dados_teste_hash.consultas_real++;
+                    }
+                 
+                exibir_dados_teste = true;
+                free(valores_teste);
+                puts("Os resultados se encontram na aba estatistica");
                 break;
             }
             default:
@@ -228,7 +328,6 @@ int main() {
         default:
             liberar_filtro(filtro);
             free(tabela.tabela);
-            free(testes_id);
 
             puts("Tchau!");
             exit(0);
