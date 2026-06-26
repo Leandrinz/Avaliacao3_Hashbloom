51372c2cf90405ffa08aa4b6383e3ed693fcb15f
Jozenias Antônio <fayrghos@proton.me>
2026-06-26 20:46:23 -0300

    Formalização de várias strings

--- programa/src/main.c
+++ programa/src/main.c
@@ -135,8 +135,6 @@ int main() {
         case 3: {
             puts("ESTATÍSTICAS:");
 
-            
-
             if(!exibir_dados_teste) {
                 if (estats.consultas_real != 0) {
                     estats.falsos_tx = (float)estats.falsos / estats.consultas_real;
@@ -151,7 +149,7 @@ int main() {
                 printf("    Tempo médio de consulta: %.1fms\n", estats.tempo_medio);
             }
             else {
-                puts("DADOS COM BLOOM:");
+                puts("  [COM BLOOM]");
                  if (dados_teste_bloom.consultas_real != 0) {
                     dados_teste_bloom.falsos_tx = (float)dados_teste_bloom.falsos / dados_teste_bloom.consultas_real;
                     dados_teste_bloom.tempo_medio = dados_teste_bloom.tempo_acc / dados_teste_bloom.consultas_real;
@@ -163,7 +161,8 @@ int main() {
                 printf("    Taxa de falsos positivos: %.2f%%\n", dados_teste_bloom.falsos_tx * 100);
                 printf("    Tempo médio de consulta: %.1fms\n", dados_teste_bloom.tempo_medio);
 
-                puts("DADOS SEM BLOOM:");
+                puts("");
+                puts("  [SEM BLOOM]");
                 if (dados_teste_hash.consultas_real != 0) {
                     dados_teste_hash.tempo_medio = dados_teste_hash.tempo_acc / dados_teste_hash.consultas_real;
                     }
@@ -171,17 +170,21 @@ int main() {
                 printf("    Consultas realizadas na tabela: %d\n", dados_teste_hash.consultas_real);
                 printf("    Tempo médio de consulta: %.1fms\n", dados_teste_hash.tempo_medio);
 
-                puts("    [1] - Parar de exibir testes e resetar suas estatisticas");
-                puts("    [2] - Sair dessa aba (qualquer tecla)");
+                puts("");
+                puts("    [1] - Resetar Estatísticas");
+                puts("    [?] - Voltar");
                 puts("");
 
                 int modo = 0;
                 printf("ESCOLHA: ");
                 scanf("%d", &modo);
-                if(modo == 1) {
+                if (modo == 1) {
                     exibir_dados_teste = false;
                     dados_teste_bloom = (Estatisticas){0};
                     dados_teste_hash = (Estatisticas){0};
+                    puts("Estatísticas resetadas!");
+                } else {
+                    puts("Voltando...");
                 }
             }
             break;
@@ -189,11 +192,10 @@ int main() {
 
         case 4: {
             //Informa quais arquivos ja foram carregados e estao no sistema
-            
 
             puts("Selecione uma das opções abaixo:");
-            puts("    [1] - Lote oficial");
-            puts("    [2] - Realizar testes em lote");
+            puts("    [1] - Carregar Lote");
+            puts("    [2] - Realizar Testes em Lote");
             puts("");
 
             int modo = 0;
@@ -239,19 +241,19 @@ int main() {
             case 2: {
                 //Criar estatistica proprias para teste de lote
                 if(!carregado_tabela) {
-                    puts("Falta os valores originais para realizar um teste");
-                    puts("Porfavor os insira na opçao 1 dessa pagina");
+                    puts("É necessário inserir os valores originais para realizar um teste.");
+                    puts("Insira os valores na opção 'Carregar Lote'.");
                     break;
                 }
                 if(exibir_dados_teste) {
-                    puts("Seu teste esta pronto, pfvr visualize na aba estatistica");
+                    puts("Verifique a seção de estatísticas para ver seu teste.");
                     break;
                 }
 
                 int tamanho_teste = 0;
                 Usuario *valores_teste;
 
-                printf("Teste: ./dados/arquivos/");
+                printf("TESTES: ./dados/arquivos/");
 
                 char arquivo[40] = "";
                 scanf("%39s", arquivo);
@@ -286,9 +288,9 @@ int main() {
 
                 puts("O arquivo foi lido com sucesso!");
                 fclose(lote);
-                          
+
                     for(int i = 0; i < tamanho_teste; i++) {
-                        // Criamos uma struct temporária 
+                        // Criamos uma struct temporária
                         Usuario temp = valores_teste[i];
 
                         if (consultar_filtro(filtro, &temp) == false) {
@@ -313,10 +315,10 @@ int main() {
                             }
                             dados_teste_hash.consultas_real++;
                     }
-                 
+
                 exibir_dados_teste = true;
                 free(valores_teste);
-                puts("Os resultados se encontram na aba estatistica");
+                puts("Os resultados podem ser vistos na seção de estatísticas.");
                 break;
             }
             default:
